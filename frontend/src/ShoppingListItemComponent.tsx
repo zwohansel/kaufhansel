4b1e585c234fd58283f522911d9153660f9dbeee
Claus Steuer <claus755@gmail.com>
2020-05-21 19:55:33 +0200

    Make shopping list item responsive

--- frontend/src/ShoppingListItemComponent.tsx
+++ frontend/src/ShoppingListItemComponent.tsx
@@ -1,5 +1,5 @@
 import { DeleteFilled } from "@ant-design/icons";
-import { AutoComplete, Button, Checkbox, List } from "antd";
+import { AutoComplete, Button, Checkbox, Col, List, Row } from "antd";
 import React, { useState } from "react";
 import { ShoppingListItem } from "./ShoppingListItem";
 import { unique } from "./utils";
@@ -32,38 +32,49 @@ export function ShoppingListItemComponent(props: ShoppingListItemComponentProps)
         textDecoration: props.item.checked ? "line-through" : "none"
       }}
     >
-      <Checkbox
-        style={{ marginRight: "1em" }}
-        checked={props.item.checked}
-        onChange={event => props.onItemCheckedChange(event.target.checked)}
-      />
-      {props.item.name}
+      <Row style={{ width: "100%" }}>
+        <Col span={22}>
+          <Checkbox
+            style={{ marginRight: "1em" }}
+            checked={props.item.checked}
+            onChange={event => props.onItemCheckedChange(event.target.checked)}
+          />
+          {props.item.name}
 
-      {props.assigneeCandidates && props.item.assignee ? " kauft" : ""}
-      {props.assigneeCandidates && (
-        <AutoComplete
-          defaultActiveFirstOption={true}
-          value={newAssigneeName}
-          onChange={setNewAssigneeName}
-          onSelect={selectNewAssignee}
-          onInputKeyDown={event => {
-            if (event.keyCode === 13) {
-              selectNewAssignee();
-            }
-          }}
-          onBlur={selectNewAssignee}
-          placeholder={"Wer kauf das?"}
-          style={{ width: 200 }}
-          options={assigneeCandidates.map(value => {
-            return { value };
-          })}
-          bordered={false}
-          size={"small"}
-        ></AutoComplete>
-      )}
-      <Button style={{ float: "right", border: "0px" }} onClick={props.onItemDeleted} data-testid="delete-item-btn">
-        <DeleteFilled alt={"Eintrag entfernen"} />
-      </Button>
+          {props.assigneeCandidates && props.item.assignee ? " kauft" : ""}
+          {props.assigneeCandidates && (
+            <AutoComplete
+              defaultActiveFirstOption={true}
+              value={newAssigneeName}
+              onChange={setNewAssigneeName}
+              onSelect={selectNewAssignee}
+              onInputKeyDown={event => {
+                if (event.keyCode === 13) {
+                  selectNewAssignee();
+                }
+              }}
+              onBlur={selectNewAssignee}
+              placeholder={"Wer kauf das?"}
+              options={assigneeCandidates.map(value => {
+                return { value };
+              })}
+              bordered={false}
+              size={"small"}
+            ></AutoComplete>
+          )}
+        </Col>
+        <Col span={2}>
+          <Button
+            type="ghost"
+            style={{ border: "0px", padding: 0 }}
+            onClick={props.onItemDeleted}
+            data-testid="delete-item-btn"
+            className="delete-item-btn"
+          >
+            <DeleteFilled alt={"Eintrag entfernen"} />
+          </Button>
+        </Col>
+      </Row>
     </List.Item>
   );
 }
