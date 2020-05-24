1504f25bd03ee30f28194fa7a7d41c31bc49a1f7
Claus Steuer <claus755@gmail.com>
2020-05-24 21:16:12 +0200

    adjust list item size

--- frontend/src/ShoppingListItemComponent.tsx
+++ frontend/src/ShoppingListItemComponent.tsx
@@ -1,5 +1,5 @@
 import { DeleteFilled } from "@ant-design/icons";
-import { AutoComplete, Button, Checkbox, List } from "antd";
+import { AutoComplete, Button, Checkbox, Col, List, Row } from "antd";
 import React, { useState } from "react";
 import { ShoppingListItem } from "./ShoppingListItem";
 import { unique } from "./utils";
@@ -35,57 +35,61 @@ export function ShoppingListItemComponent(props: ShoppingListItemComponentProps)
         textDecoration: props.item.checked ? "line-through" : "none"
       }}
       onClick={() => props.onItemCheckedChange(!props.item.checked)}
-      actions={[
-        <Button
-          key="delete-item-btn"
-          type="dashed"
-          size="small"
-          loading={deletingItem}
-          onClick={async e => {
-            e.stopPropagation();
-            setDeletingItem(true);
-            await props.onItemDeleted();
-            setDeletingItem(false);
-          }}
-          data-testid="delete-item-btn"
-          className="delete-item-btn"
-        >
-          {deletingItem ? <div /> : <DeleteFilled alt={"Eintrag entfernen"} />}
-        </Button>
-      ]}
     >
-      <Checkbox
-        style={{ marginRight: "1em" }}
-        checked={props.item.checked}
-        onChange={event => props.onItemCheckedChange(event.target.checked)}
-      />
-      {props.item.name}
+      <Row className="shopping-list-item-row">
+        <Col span={20}>
+          <Checkbox
+            style={{ marginRight: "1em" }}
+            checked={props.item.checked}
+            onChange={event => props.onItemCheckedChange(event.target.checked)}
+          />
+          {props.item.name}
 
-      {props.assigneeCandidates && props.item.assignee ? " kauft" : ""}
-      {props.assigneeCandidates && (
-        <AutoComplete
-          defaultActiveFirstOption={true}
-          value={newAssigneeName}
-          onChange={setNewAssigneeName}
-          onSelect={selectNewAssignee}
-          onClick={e => {
-            e.stopPropagation();
-          }}
-          onInputKeyDown={event => {
-            if (event.keyCode === 13) {
-              selectNewAssignee();
-            }
-          }}
-          onBlur={selectNewAssignee}
-          placeholder={"Wer kauf das?"}
-          options={assigneeCandidates.map(value => {
-            return { value };
-          })}
-          bordered={false}
-          size={"small"}
-          style={{ minWidth: "30%" }}
-        ></AutoComplete>
-      )}
+          {props.assigneeCandidates && props.item.assignee ? " kauft" : ""}
+          {props.assigneeCandidates && (
+            <AutoComplete
+              defaultActiveFirstOption={true}
+              value={newAssigneeName}
+              onChange={setNewAssigneeName}
+              onSelect={selectNewAssignee}
+              onClick={e => {
+                e.stopPropagation();
+              }}
+              onInputKeyDown={event => {
+                if (event.keyCode === 13) {
+                  selectNewAssignee();
+                }
+              }}
+              onBlur={selectNewAssignee}
+              placeholder={"Wer kauf das?"}
+              options={assigneeCandidates.map(value => {
+                return { value };
+              })}
+              bordered={false}
+              size={"small"}
+              style={{ minWidth: "30%" }}
+            ></AutoComplete>
+          )}
+        </Col>
+        <Col span={4}>
+          <Button
+            key="delete-item-btn"
+            type="dashed"
+            size="small"
+            loading={deletingItem}
+            onClick={async e => {
+              e.stopPropagation();
+              setDeletingItem(true);
+              await props.onItemDeleted();
+              setDeletingItem(false);
+            }}
+            data-testid="delete-item-btn"
+            className="delete-item-btn"
+          >
+            {deletingItem ? <div /> : <DeleteFilled alt={"Eintrag entfernen"} />}
+          </Button>
+        </Col>
+      </Row>
     </List.Item>
   );
 }
