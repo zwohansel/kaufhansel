c6c4c25108ad55ad0955edfbb181f03efa32f4a2
Claus Steuer <claus755@gmail.com>
2020-05-29 20:50:17 +0200

    Make assignee selection mobile friendly

--- frontend/src/ShoppingListItemComponent.tsx
+++ frontend/src/ShoppingListItemComponent.tsx
@@ -1,6 +1,6 @@
 import { DeleteFilled } from "@ant-design/icons";
-import { AutoComplete, Button, Checkbox, Col, List, Row } from "antd";
-import React, { useState } from "react";
+import { Button, Checkbox, Col, Input, List, Modal, Row } from "antd";
+import React, { Fragment, useState } from "react";
 import { ShoppingListItem } from "./ShoppingListItem";
 import { unique } from "./utils";
 
@@ -13,78 +13,139 @@ export interface ShoppingListItemComponentProps {
 }
 
 export function ShoppingListItemComponent(props: ShoppingListItemComponentProps) {
-  const [newAssigneeName, setNewAssigneeName] = useState(props.item.assignee);
+  const assigneeCandidates = props.assigneeCandidates ? unique(props.assigneeCandidates.filter(e => e !== "")) : [];
 
-  const assigneeCandidates = props.assigneeCandidates
-    ? unique(props.assigneeCandidates.filter(e => e.startsWith(newAssigneeName) && e !== ""))
-    : [];
+  const [deletingItem, setDeletingItem] = useState(false);
+  const [newAssignee, setNewAssignee] = useState("");
+  const [showNewAssigneeSelection, setShowNewAssigneeSelection] = useState(false);
 
-  const selectNewAssignee = () => {
-    if (props.onItemAssigneeChange && newAssigneeName !== props.item.assignee) {
-      props.onItemAssigneeChange(newAssigneeName);
+  const handleAssigneeSelectionOk = () => {
+    setShowNewAssigneeSelection(false);
+
+    if (props.onItemAssigneeChange) {
+      props.onItemAssigneeChange(newAssignee);
     }
   };
 
-  const [deletingItem, setDeletingItem] = useState(false);
+  const handleAssigneeSelectionCancel = () => {
+    setShowNewAssigneeSelection(false);
+  };
 
   return (
-    <List.Item
-      className="shopping-list-item"
-      key={props.item._id}
-      style={{
-        textDecoration: props.item.checked ? "line-through" : "none"
-      }}
-      onClick={() => props.onItemCheckedChange(!props.item.checked)}
-    >
-      <Row className="shopping-list-item-row">
-        <Col span={20}>
-          <Checkbox style={{ marginRight: "1em" }} checked={props.item.checked} />
-          {props.item.name}
-          {props.assigneeCandidates && props.item.assignee ? " kauft" : ""}
-          {props.assigneeCandidates && (
-            <AutoComplete
-              defaultActiveFirstOption={true}
-              value={newAssigneeName}
-              onChange={setNewAssigneeName}
-              onSelect={selectNewAssignee}
-              onClick={e => {
-                e.stopPropagation();
-              }}
-              onInputKeyDown={event => {
-                if (event.keyCode === 13) {
-                  selectNewAssignee();
+    <Fragment>
+      <List.Item
+        className="shopping-list-item"
+        key={props.item._id}
+        style={{
+          textDecoration: props.item.checked ? "line-through" : "none"
+        }}
+        onClick={() => props.onItemCheckedChange(!props.item.checked)}
+      >
+        <Row className="shopping-list-item-row">
+          <Col span={20}>
+            <Checkbox style={{ marginRight: "1em" }} checked={props.item.checked} />
+            {props.item.name}
+            {props.assigneeCandidates && props.item.assignee ? " kauft" : ""}
+            {props.assigneeCandidates && (
+              <span
+                className={
+                  "assignee-candidate" +
+                  (props.item.assignee === "" ? " assignee-candidate-placeholder" : " assignee-candidate-set")
                 }
+                onClick={e => {
+                  e.stopPropagation();
+                  setNewAssignee("");
+                  setShowNewAssigneeSelection(true);
+                }}
+              >
+                {props.item.assignee === "" ? "Wer kauft das?" : props.item.assignee}
+              </span>
+            )}
+          </Col>
+          <Col span={4}>
+            <Button
+              key="delete-item-btn"
+              type="dashed"
+              size="small"
+              loading={deletingItem}
+              onClick={async e => {
+                e.stopPropagation();
+                setDeletingItem(true);
+                await props.onItemDeleted();
+                setDeletingItem(false);
               }}
-              onBlur={selectNewAssignee}
-              placeholder={"Wer kauft das?"}
-              options={assigneeCandidates.map(value => {
-                return { value };
-              })}
-              bordered={false}
-              size={"small"}
-              style={{ minWidth: "30%" }}
-            ></AutoComplete>
-          )}
-        </Col>
-        <Col span={4}>
+              data-testid="delete-item-btn"
+              className="delete-item-btn"
+            >
+              {deletingItem ? <div /> : <DeleteFilled alt={"Eintrag entfernen"} />}
+            </Button>
+          </Col>
+        </Row>
+      </List.Item>
+      <Modal
+        title={
+          <div>
+            <span>Wer kauft das?</span>
+            <span className="assignee-select-dialog-subtitle">Wählstu oder sagstu!</span>
+          </div>
+        }
+        visible={showNewAssigneeSelection}
+        okButtonProps={{ disabled: newAssignee === "" }}
+        onOk={handleAssigneeSelectionOk}
+        onCancel={handleAssigneeSelectionCancel}
+        footer={[
+          <Button key="cancel" onClick={handleAssigneeSelectionCancel}>
+            Egal
+          </Button>,
           <Button
-            key="delete-item-btn"
-            type="dashed"
-            size="small"
-            loading={deletingItem}
-            onClick={async e => {
-              e.stopPropagation();
-              setDeletingItem(true);
-              await props.onItemDeleted();
-              setDeletingItem(false);
+            key="delete"
+            danger
+            disabled={props.item.assignee === ""}
+            onClick={() => {
+              setShowNewAssigneeSelection(false);
+
+              if (props.onItemAssigneeChange) {
+                props.onItemAssigneeChange("");
+              }
             }}
-            data-testid="delete-item-btn"
-            className="delete-item-btn"
           >
-            {deletingItem ? <div /> : <DeleteFilled alt={"Eintrag entfernen"} />}
+            Niemand
+          </Button>,
+          <Button key="ok" type="primary" onClick={handleAssigneeSelectionOk}>
+            Zuweisen
           </Button>
-        </Col>
-      </Row>
-    </List.Item>
+        ]}
+      >
+        {assigneeCandidates.length !== 0 ? (
+          <List
+            dataSource={assigneeCandidates}
+            split={false}
+            renderItem={assignee => (
+              <List.Item
+                className={"assignee-candidate-item" + (props.item.assignee === assignee ? " current-candidate" : "")}
+                key={assignee}
+                onClick={() => {
+                  setShowNewAssigneeSelection(false);
+
+                  if (props.onItemAssigneeChange) {
+                    props.onItemAssigneeChange(assignee);
+                  }
+                }}
+              >
+                {assignee}
+              </List.Item>
+            )}
+          />
+        ) : (
+          <div />
+        )}
+        <Input
+          placeholder={"Sagstu!"}
+          value={newAssignee}
+          onChange={e => setNewAssignee(e.target.value)}
+          onPressEnter={handleAssigneeSelectionOk}
+        />
+      </Modal>
+    </Fragment>
   );
 }
