46ad7073c9ef3fcddf99ff36ad49106866c705e6
Claus Steuer <claus755@gmail.com>
2020-06-04 20:01:35 +0200

    Fix cross build with qt

--- status_display/raspberrypi-gcc.cmake
+++ status_display/raspberrypi-gcc.cmake
@@ -4,9 +4,4 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
 
-set(CMAKE_SYSROOT /home/michael/workspaces/raspi/sysroot)
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_SYSROOT /opt/raspi/sysroot)
