1a8d6c48975a0a4c0dd12fc32711da50999f18c3
Claus Steuer <claus755@gmail.com>
2024-03-20 20:04:22 +0100

    frontend: fix missing permission on android and confirm dialog on mobile

--- flutter_frontend/lib/widgets/confirm_dialog.dart
+++ flutter_frontend/lib/widgets/confirm_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:kaufhansel_client/generated/l10n.dart';
 
 Future<bool?> showConfirmDialog(BuildContext context, String text,
@@ -7,6 +6,7 @@ Future<bool?> showConfirmDialog(BuildContext context, String text,
     String? confirmBtnLabel,
     String? cancelBtnLabel,
     Color? confirmBtnColor,
+    Color? confirmBtnFontColor,
     bool hideCancelBtn = false}) {
   final displayTitle = title == null ? AppLocalizations.of(context).important : title;
   final displayConfirmBtnLabel = confirmBtnLabel == null ? AppLocalizations.of(context).ok : confirmBtnLabel;
@@ -16,7 +16,14 @@ Future<bool?> showConfirmDialog(BuildContext context, String text,
     barrierDismissible: false,
     context: context,
     builder: (context) => _ConfirmationDialog(
-        displayTitle, text, displayConfirmBtnLabel, confirmBtnColor ?? Colors.red, displayCancelBtnLabel, hideCancelBtn),
+      displayTitle,
+      text,
+      displayConfirmBtnLabel,
+      confirmBtnColor ?? Colors.red,
+      confirmBtnFontColor ?? Colors.white,
+      displayCancelBtnLabel,
+      hideCancelBtn,
+    ),
   );
 }
 
@@ -25,11 +32,12 @@ class _ConfirmationDialog extends StatelessWidget {
   final String _text;
   final String _confirmBtnLabel;
   final Color _confirmBtnColor;
+  final Color _confirmBtnFontColor;
   final String _cancelBtnLabel;
   final bool _hideCancelBtn;
 
-  const _ConfirmationDialog(
-      this._title, this._text, this._confirmBtnLabel, this._confirmBtnColor, this._cancelBtnLabel, this._hideCancelBtn);
+  const _ConfirmationDialog(this._title, this._text, this._confirmBtnLabel, this._confirmBtnColor,
+      this._confirmBtnFontColor, this._cancelBtnLabel, this._hideCancelBtn);
 
   @override
   Widget build(BuildContext context) {
@@ -43,13 +51,13 @@ class _ConfirmationDialog extends StatelessWidget {
           ),
         ),
       );
-      buttonRow.add(SizedBox(width: 10));
+      buttonRow.add(SizedBox(width: 10, height: 10));
     }
     buttonRow.add(
       ElevatedButton(
         style: ElevatedButton.styleFrom(backgroundColor: _confirmBtnColor),
         onPressed: () => Navigator.pop(context, true),
-        child: Text(_confirmBtnLabel),
+        child: Text(_confirmBtnLabel, style: TextStyle(color: _confirmBtnFontColor)),
       ),
     );
 
@@ -57,13 +65,18 @@ class _ConfirmationDialog extends StatelessWidget {
       Padding(
           padding: EdgeInsets.only(left: 24, right: 24, top: 12),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
             children: [
               Text(_text),
               SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: _hideCancelBtn ? MainAxisAlignment.center : MainAxisAlignment.spaceBetween,
-                children: buttonRow,
+              IntrinsicHeight(
+                child: Flex(
+                  direction: MediaQuery.of(context).size.width > 400 ? Axis.horizontal : Axis.vertical,
+                  mainAxisAlignment: _hideCancelBtn ? MainAxisAlignment.center : MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: buttonRow,
+                ),
               )
             ],
           ))
